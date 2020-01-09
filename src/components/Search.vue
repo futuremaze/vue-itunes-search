6ead968ad9a3a1c5c34a364d2825324cf3dca868
futuremaze <future.maze@gmail.com>
2020-01-09 10:40:14 +0900

    検索処理ロジックを実装

---
+++ src/components/Search.vue
@@ -0,0 +1,20 @@
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      term: ""
+    };
+  },
+  methods: {
+    async exe() {
+      this.$emit("loadStart");
+      const { data } = await axios.get(
+        `//itunes.apple.com/search?term=${this.term}&country=jp&entity=musicVideo`
+      );
+      this.$emit("loadComplete", { results: data.results });
+    }
+  }
+};
+</script>
